a9d8abe8f4e6a5842e6887fbefbe46ec83f1e11d
FilipStenbeck <filip.stenbeck@gmail.com>
2013-06-18 16:28:41 +0200

    some minor tweeks

--- js/a.js
+++ js/a.js
@@ -1,4 +1,5 @@
 //GLOBAL namespace
 var app = {};
-
+//This is first part of the message
 app.message1 = "I use Grunt...";
+
--- js/b.js
+++ js/b.js
@@ -1,1 +1,2 @@
+//This is the second part of the message
 app.message2 = "and I like it!";
