64eb9af0113b72ecfefbe7aa4032ed763564f136
FilipStenbeck <filip.stenbeck@gmail.com>
2013-09-07 09:38:51 +0200

    added test

--- test/dude_test.js
+++ test/dude_test.js
@@ -6,3 +6,13 @@ describe('The dude speaks', function () {
         expect(quote.length).toBeGreaterThan(0);
     });  
 });
+
+
+describe('The dude never speaks the same', function () {
+    it('should never genereate the same quote twize in a row', function () {
+        var dude = new Dude();
+        var quote1 = dude.speak();
+        var quote2 = dude.speak();
+        expect(quote1).not.toEqual(quote2);
+    });  
+});
