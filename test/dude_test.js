a093c449a7073ab2928d6f61a51349d698678029
FilipStenbeck <filip.stenbeck@gmail.com>
2013-09-07 14:29:06 +0200

    Both test specs in the same suit

--- test/dude_test.js
+++ test/dude_test.js
@@ -1,18 +1,15 @@
-describe('The dude speaks', function () {
+describe('The dude speaks ', function () {
+    var dude;
+    
+    beforeEach(function() {
+        dude = new Dude();
+    });
+
     it('should speak slacker wisdoms', function () {
-        var dude = new Dude();
-        var quote = dude.speak();
-        console.log(quote); 
-        expect(quote.length).toBeGreaterThan(0);
-    });  
-});
+        expect(dude.speak().length).toBeGreaterThan(0);
+    });
 
-
-describe('The dude never speaks the same', function () {
-    it('should never genereate the same quote twize in a row', function () {
-        var dude = new Dude();
-        var quote1 = dude.speak();
-        var quote2 = dude.speak();
-        expect(quote1).not.toEqual(quote2);
-    });  
+    it('should never genereate the same quote twize in a row', function () {   
+        expect(dude.speak()).not.toEqual(dude.speak());
+    });    
 });
